a2d6e82aedafe578581d83d3c6d1af17abff01ac
Chris Nelson <cnelson04@gmail.com>
2021-08-30 19:51:37 -0500

    capture an input

--- Scripts/client.js
+++ Scripts/client.js
@@ -3,9 +3,20 @@ $( document ).ready( onReady );
 
 function onReady(){
     console.log( 'JQuery is Ready!' ); // using backtick, not single quotes
-    // targe an element and put some text in it.
+    showText();
+} // end onReady
+
+function showText(){
+      // targe an element and put some text in it.
     // target an element and hold it in a variable
     let el = $( '#hello' ); // # = id
-    el.empty(); // empty target "el" variable
+    el.empty(); // empty target "el" variable ('thingy' from html doc )
     el.append( 'Hello, world!'); // append text into el
-} // end onReady
+} // end showText
+
+function getText(){
+    // target an element by ID and get its .val()
+    let texter = $( '#textIn' ).val();
+    // console log the value
+    console.log( `You typed:`, texter )
+} // end getText
