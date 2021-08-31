c8c113b7b64276a404aa6e221e629f67d5997506
Chris Nelson <cnelson04@gmail.com>
2021-08-30 20:11:49 -0500

    button and text field

--- Scripts/client.js
+++ Scripts/client.js
@@ -3,20 +3,22 @@ $( document ).ready( onReady );
 
 function onReady(){
     console.log( 'JQuery is Ready!' ); // using backtick, not single quotes
-    showText();
+    // click on a button with an ID, run a function
+    $( '#getTextButton' ).on( 'click', getText );
 } // end onReady
 
-function showText(){
+function showText( textToShow ){
       // targe an element and put some text in it.
     // target an element and hold it in a variable
     let el = $( '#hello' ); // # = id
     el.empty(); // empty target "el" variable ('thingy' from html doc )
-    el.append( 'Hello, world!'); // append text into el
+    el.append( textToShow ); // append text into el
 } // end showText
 
 function getText(){
     // target an element by ID and get its .val()
     let texter = $( '#textIn' ).val();
-    // console log the value
-    console.log( `You typed:`, texter )
+    $('#textIn' ).val (''); // setter
+    // console log the value`
+    showText( `You typed: ${ texter }` );
 } // end getText
