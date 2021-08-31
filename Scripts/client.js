c408c0c4e45d59fee09ed4c3a884a0025dd97104
Chris Nelson <cnelson04@gmail.com>
2021-08-30 19:35:15 -0500

    first element created

--- Scripts/client.js
+++ Scripts/client.js
@@ -3,4 +3,9 @@ $( document ).ready( onReady );
 
 function onReady(){
     console.log( 'JQuery is Ready!' ); // using backtick, not single quotes
+    // targe an element and put some text in it.
+    // target an element and hold it in a variable
+    let el = $( '#hello' ); // # = id
+    el.empty(); // empty target "el" variable
+    el.append( 'Hello, world!'); // append text into el
 } // end onReady
